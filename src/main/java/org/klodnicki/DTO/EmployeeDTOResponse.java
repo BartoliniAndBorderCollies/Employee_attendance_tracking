3f3bbbd0cc06b2877d02bc013761f30341aa5865
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-02 12:04:04 +0200

    add EmployeeDTOResponse class

---
+++ src/main/java/org/klodnicki/DTO/EmployeeDTOResponse.java
@@ -0,0 +1,18 @@
+package org.klodnicki.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.klodnicki.model.Department;
+import org.klodnicki.model.Salary;
+
+@Getter
+@Setter
+public class EmployeeDTOResponse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Department department;
+    private Salary salary;
+}
