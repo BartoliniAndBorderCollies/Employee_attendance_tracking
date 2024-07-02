96e8ad066f695d4875224803bfc13ffd1255b939
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-02 10:59:46 +0200

    add @Embeddable to Salary class

--- src/main/java/org/klodnicki/model/Salary.java
+++ src/main/java/org/klodnicki/model/Salary.java
@@ -1,10 +1,12 @@
 package org.klodnicki.model;
 
+import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Embeddable
 public class Salary {
 
     private double amount;
