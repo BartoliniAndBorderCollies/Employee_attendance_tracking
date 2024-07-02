e4ca335492c3651ddd3ae1bbe68617027e174bbd
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-02 12:03:41 +0200

    add EmployeeRepository

---
+++ src/main/java/org/klodnicki/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package org.klodnicki.repository;
+
+import org.klodnicki.model.entity.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+}
