40eebe8aecde9207175ebf3d11c5e57cc784e6fb
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-05 11:09:29 +0200

    add SearchController with findByName()

---
+++ src/main/java/org/klodnicki/rest/controller/SearchController.java
@@ -0,0 +1,26 @@
+package org.klodnicki.rest.controller;
+
+import lombok.AllArgsConstructor;
+import org.klodnicki.DTO.Employee.EmployeeDTOResponse;
+import org.klodnicki.service.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/search")
+@AllArgsConstructor
+public class SearchController {
+
+    private final EmployeeService employeeService;
+
+    @GetMapping("/name")
+    public List<EmployeeDTOResponse> findByName (@RequestParam("lastName") String lastName) {
+        return employeeService.findByName(lastName);
+    }
+
+
+}
