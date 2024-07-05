04f21f7978133a879dd28565c75345a06edb8fc1
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-05 16:05:26 +0200

    add Junit test to check findByName()

--- src/test/java/org/klodnicki/service/EmployeeServiceTest.java
+++ src/test/java/org/klodnicki/service/EmployeeServiceTest.java
@@ -130,7 +130,7 @@ class EmployeeServiceTest {
         //Arrange
         //Act
         //Assert
-        assertThrows(NotFoundInDatabaseException.class, ()-> employeeService.update(nonExistentId, employeeDTORequest));
+        assertThrows(NotFoundInDatabaseException.class, () -> employeeService.update(nonExistentId, employeeDTORequest));
     }
 
     @Test
@@ -158,7 +158,7 @@ class EmployeeServiceTest {
         //Arrange
         //Act
         //Assert
-        assertThrows(NotFoundInDatabaseException.class, ()-> employeeService.delete(nonExistentId));
+        assertThrows(NotFoundInDatabaseException.class, () -> employeeService.delete(nonExistentId));
     }
 
     @Test
@@ -182,6 +182,28 @@ class EmployeeServiceTest {
         //Verify
         verify(employeeRepository).findById(existingId);
         verify(employeeRepository).delete(employee);
+    }
+
+    @Test
+    public void findByName_ShouldFindAndMapAndReturnEmployeeDTOList_WhenLastNameIsGiven() {
+        //Arrange
+        Employee employee1 = new Employee();
+        List<Employee> employeeList = Arrays.asList(employee1);
+        List<EmployeeDTOResponse> expected = Arrays.asList(employeeDTOResponse);
+
+        when(employeeRepository.findByLastName(employee1.getLastName())).thenReturn(employeeList);
+        when(modelMapper.map(employee1, EmployeeDTOResponse.class)).thenReturn(employeeDTOResponse);
+
+        //Act
+        List<EmployeeDTOResponse> actualResponse = employeeService.findByName(employee1.getLastName());
+
+        //Assert
+        assertNotNull(actualResponse, "Actual response should not be null");
+        assertEquals(expected, actualResponse);
+
+        //Verify
+        verify(employeeRepository).findByLastName(employee1.getLastName());
+        verify(modelMapper).map(employee1, EmployeeDTOResponse.class);
     }
 
 
