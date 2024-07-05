5f3d2af1bbfe3041e865529cbbaf24f6fcf1291c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-05 10:03:42 +0200

    delete constraint and add @Data to Salary class

--- src/main/java/org/klodnicki/model/Salary.java
+++ src/main/java/org/klodnicki/model/Salary.java
@@ -1,19 +1,15 @@
 package org.klodnicki.model;
 
 import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class Salary {
 
-    @Min(value = 0, message = "Salary must be positive!")
     private double amount;
 }
