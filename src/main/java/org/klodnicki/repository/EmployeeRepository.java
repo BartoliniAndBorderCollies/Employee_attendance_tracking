109b1a41614abf61eda4758cfe3f0e1725c994f1
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-05 11:10:15 +0200

    add findByName() to EmployeeRepository

--- src/main/java/org/klodnicki/repository/EmployeeRepository.java
+++ src/main/java/org/klodnicki/repository/EmployeeRepository.java
@@ -4,9 +4,11 @@ import org.klodnicki.model.entity.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
     Optional<Employee> findById(Long id);
+    List<Employee> findByLastName(String lastName);
 }
