e883d717c9c0822a3e38f905d27b960b853b85c0
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-16 10:34:05 +0200

    delete of findById() from EmployeeRepository (it is already in CrudRepository interface

--- src/main/java/org/klodnicki/repository/EmployeeRepository.java
+++ src/main/java/org/klodnicki/repository/EmployeeRepository.java
@@ -8,11 +8,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-    Optional<Employee> findById(Long id);
     List<Employee> findByLastName(String lastName);
     @Query("SELECT e FROM Employee e WHERE e.salary.amount >= :from AND e.salary.amount < :to")
     List<Employee> findBySalaryRange(@Param("from") double from, @Param("to") double to);
