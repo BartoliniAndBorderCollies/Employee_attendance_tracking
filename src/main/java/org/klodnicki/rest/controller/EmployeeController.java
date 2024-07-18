1622da215094c61d705d77d96e9b81b029b00993
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-18 13:49:25 +0200

    move methods from SearchController to EmployeeController

--- src/main/java/org/klodnicki/rest/controller/EmployeeController.java
+++ src/main/java/org/klodnicki/rest/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.klodnicki.dto.employee.EmployeeDTORequest;
 import org.klodnicki.dto.employee.EmployeeDTOResponse;
 import org.klodnicki.dto.ResponseDTO;
 import org.klodnicki.exception.NotFoundInDatabaseException;
+import org.klodnicki.model.Department;
 import org.klodnicki.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -44,6 +45,39 @@ public class EmployeeController {
     @DeleteMapping("/{id}")
     public ResponseDTO deleteById(@PathVariable Long id) throws NotFoundInDatabaseException {
         return employeeService.delete(id);
+    }
+
+    /**
+     * Finds employees by their last name.
+     *
+     * @param lastName The last name of the employees to search for.
+     * @return A list of employees matching the given last name.
+     */
+    @GetMapping("/name")
+    public List<EmployeeDTOResponse> findByName (@RequestParam("lastName") String lastName) {
+        return employeeService.findByName(lastName);
+    }
+
+    /**
+     * Finds employees within a specified salary range.
+     *
+     * @param from The minimum salary.
+     * @param to The maximum salary.
+     * @return A list of employees whose salaries fall within the specified range.
+     */
+    @GetMapping("/salary")
+    public List<EmployeeDTOResponse> findBySalaryRange(@RequestParam("from") double from, @RequestParam("to") double to) {
+        return employeeService.findBySalaryRange(from, to);
+    }
+    /**
+     * Finds employees by their department.
+     *
+     * @param department The department to search for employees in.
+     * @return A list of employees belonging to the specified department.
+     */
+    @GetMapping("/department")
+    public List<EmployeeDTOResponse> findByDepartment (@RequestParam("department") Department department) {
+        return employeeService.findByDepartment(department);
     }
 }
 
