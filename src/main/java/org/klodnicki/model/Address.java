3439a66228e4a2048b55b8fed14975004f627247
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-24 12:21:58 +0200

    add constructor annotations to Address class

--- src/main/java/org/klodnicki/model/Address.java
+++ src/main/java/org/klodnicki/model/Address.java
@@ -1,12 +1,16 @@
 package org.klodnicki.model;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 public class Address {
 
     private String street;
