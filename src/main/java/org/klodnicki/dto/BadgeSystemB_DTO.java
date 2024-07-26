7d8ff9bf19e7f7a97e67dcf2326b6de5c85b9e85
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-26 10:53:07 +0200

    add BadgeSystemB_DTO class

---
+++ src/main/java/org/klodnicki/dto/BadgeSystemB_DTO.java
@@ -0,0 +1,20 @@
+package org.klodnicki.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.klodnicki.dto.employee.EmployeeDTOResponse;
+import org.klodnicki.model.Action;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class BadgeSystemB_DTO {
+
+    private String badgeNumber;
+    private String location;
+    private String deviceName;
+    private Action action;
+    private LocalDateTime timeStamp;
+    private EmployeeDTOResponse employee;
+}
