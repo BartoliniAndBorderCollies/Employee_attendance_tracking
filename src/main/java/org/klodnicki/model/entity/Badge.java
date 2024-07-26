bd5fbe61f36439671da6545a2ae10e8a837cdf51
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-07-26 10:50:49 +0200

    add Badge class

---
+++ src/main/java/org/klodnicki/model/entity/Badge.java
@@ -0,0 +1,31 @@
+package org.klodnicki.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.klodnicki.model.Action;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Badge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String badgeNumber;
+    private String location;
+    private String deviceName;
+    @Enumerated(EnumType.STRING)
+    private Action action;
+    private LocalDateTime timeStamp;
+    @OneToOne
+    private Employee employee;
+
+}
