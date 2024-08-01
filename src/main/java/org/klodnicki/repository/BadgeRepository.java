855aa945c8a8bb6dde723b421081dcf99663fc36
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-01 13:43:58 +0200

    add findByBadgeNumber() in BadgeRepository

--- src/main/java/org/klodnicki/repository/BadgeRepository.java
+++ src/main/java/org/klodnicki/repository/BadgeRepository.java
@@ -4,6 +4,10 @@ import org.klodnicki.model.entity.Badge;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BadgeRepository extends CrudRepository<Badge, Long> {
+
+    Optional<Badge> findByBadgeNumber(String badgeNumber);
 }
