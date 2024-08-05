49ff5ebb3bc3d15d424de19cd3dbea919dbf1d00
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-08-05 12:14:25 +0200

    add BadgeScanHistoryRepository

---
+++ src/main/java/org/klodnicki/repository/BadgeScanHistoryRepository.java
@@ -0,0 +1,14 @@
+package org.klodnicki.repository;
+
+import org.klodnicki.model.entity.Badge;
+import org.klodnicki.model.entity.BadgeScanHistory;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BadgeScanHistoryRepository extends CrudRepository<BadgeScanHistory, Long> {
+
+    Optional<BadgeScanHistory> findFirstByBadgeOrderByTimeStampDesc(Badge badge);
+}
