eb9bb1b8610275fd6ea67e33236f11894559952a
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-09-05 12:55:28 +0200

    add constructors to BadgeSystemA_DTO

--- src/main/java/org/klodnicki/dto/badge/BadgeSystemA_DTO.java
+++ src/main/java/org/klodnicki/dto/badge/BadgeSystemA_DTO.java
@@ -1,11 +1,15 @@
 package org.klodnicki.dto.badge;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.klodnicki.dto.employee.EmployeeDTOResponse;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BadgeSystemA_DTO {
 
     private String badgeNumber;
