e12cc9ec1a689a4a548d364c05804aa42bf7712d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-09-11 11:54:07 +0200

    add CSVUtil with export and import generic methods

---
+++ src/main/java/org/klodnicki/util/CSVUtil.java
@@ -0,0 +1,30 @@
+package org.klodnicki.util;
+
+import com.opencsv.bean.*;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+import java.io.*;
+import java.util.List;
+
+public class CSVUtil {
+
+    //this method will work with any type <T>, for example: List<Employee> T would be Employee, List<Badge> T would be Badge
+    public static <T> void exportToCSV(String fileName, List<T> list) throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
+        try (Writer writer = new FileWriter(fileName)) {
+            StatefulBeanToCsv<T> beanToCsv = new StatefulBeanToCsvBuilder<T>(writer).build();
+            beanToCsv.write(list);
+        }
+    }
+
+    public static <T> List<T> importFromCSV(String fileName, Class<T> clazz) throws IOException {
+        try (Reader reader = new FileReader(fileName)) {
+            CsvToBean<T> csvToBean = new CsvToBeanBuilder<T>(reader)
+                    .withType(clazz)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            return csvToBean.parse();
+        }
+    }
+}
+
