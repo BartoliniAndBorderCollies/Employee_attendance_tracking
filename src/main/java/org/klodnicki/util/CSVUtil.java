571adccb2065725d1e0a17cb59566e772d2f103d
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-09-12 11:21:09 +0200

    refactor exportToCSV() in CSVUtil -> implementation of mappingStrategy

--- src/main/java/org/klodnicki/util/CSVUtil.java
+++ src/main/java/org/klodnicki/util/CSVUtil.java
@@ -10,9 +10,12 @@ import java.util.List;
 public class CSVUtil {
 
     //this method will work with any type <T>, for example: List<Employee> T would be Employee, List<Badge> T would be Badge
-    public static <T> void exportToCSV(String fileName, List<T> list) throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
-        try (Writer writer = new FileWriter(fileName)) {
-            StatefulBeanToCsv<T> beanToCsv = new StatefulBeanToCsvBuilder<T>(writer).build();
+    public static <T> void exportToCSV(String fileName, List<T> list, ColumnPositionMappingStrategy<T> strategy)
+            throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            StatefulBeanToCsv<T> beanToCsv = new StatefulBeanToCsvBuilder<T>(writer)
+                    .withMappingStrategy(strategy)
+                    .build();
             beanToCsv.write(list);
         }
     }
