a72eb20d70edfa36344a20d7c3fda6fd694e4b74
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-09-27 12:38:34 +0200

    refactor of setUp() in EmployeeServiceTest -> add new fields

--- src/test/java/org/klodnicki/service/EmployeeServiceTest.java
+++ src/test/java/org/klodnicki/service/EmployeeServiceTest.java
@@ -50,11 +50,30 @@ class EmployeeServiceTest {
 
     @BeforeEach
     public void setUp() {
-        employeeDTORequest = new EmployeeDTORequest("firstName", "lastName", "email@email.com", Department.DEPARTMENT1,
-                new Salary(100.00), "Warsaw", LocalDate.of(1957, 12, 10), Gender.FEMALE, new Address(),
-                "telephone 123443", "bank account 321", "nip123321",
-                LocalDate.of(1998, 10, 11), new BadgeSystemB_DTO());
+        employeeDTORequest = new EmployeeDTORequest(
+                "firstName",
+                "lastName",
+                "email@email.com",
+                Department.DEPARTMENT1,
+                100.00,  // Flattened salaryAmount
+                "Warsaw",  // Birth place
+                "1957-12-10",  // rawBirthDate (String format)
+                "1998-10-11",  // rawDateOfEmployment (String format)
+                LocalDate.of(1957, 12, 10),  // birthDate
+                LocalDate.of(1998, 10, 11),  // dateOfEmployment
+                Gender.FEMALE,
+                "Street name",  // Street (Address flattened fields)
+                "12A",  // House number
+                "11-011",  // Postal code
+                "Poznan",  // City
+                "Poland",  // Country
+                "telephone 123443",
+                "bank account 321",
+                "nip123321",
+                new BadgeSystemB_DTO()  // Badge
+        );
     }
+
 
     @Test
     public void create_ShouldMapSaveAndMapAgain_WhenEmployeeDTORequestIsGiven() {
