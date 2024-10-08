1a95b96c04df54f8d57dfdecb3d26214188316fe
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2024-10-08 09:44:14 +0200

    delete of unused fields

--- src/main/java/org/klodnicki/dto/badge/BadgeSystemA_DTO.java
+++ src/main/java/org/klodnicki/dto/badge/BadgeSystemA_DTO.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.klodnicki.model.Action;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -17,6 +14,4 @@ public class BadgeSystemA_DTO {
     private String badgeNumber;
     private String location;
     private String deviceName;
-    private Action action;
-    private LocalDateTime timeStamp;
 }
